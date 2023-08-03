94380477aec646f7a109eaaee35f36ace204e48e
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2023-08-03 17:17:01 -0300

    Add project files.

---
+++ DesignPatterns2/Factory/ConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DesignPatterns2.Cap1
+{
+    public class ConnectionFactory
+    {
+        public IDbConnection GetConnection()
+        {
+            IDbConnection conexao = new SqlConnection();
+            conexao.ConnectionString = "User ID=root; Password=; Serve=localhost; Database=meuBanco";
+            conexao.Open();
+
+            return conexao;
+        }
+    }
+}
---
+++ DesignPatterns2/Program.cs
@@ -0,0 +1,7 @@
+﻿using DesignPatterns2.Cap1;
+using System.Data;
+
+IDbConnection conexao = new ConnectionFactory().GetConnection();
+
+IDbCommand comando =  conexao.CreateCommand();
+comando.CommandText = "select * from table";
