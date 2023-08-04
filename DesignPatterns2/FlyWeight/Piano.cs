a9596c3532c1cd55721f0d8ede2bd97271bbc01a
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2023-08-04 15:56:13 -0300

    estudo do padrão flyweight

---
+++ DesignPatterns2/FlyWeight/Piano.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatterns2.Cap2
+{
+    public class Piano
+    {
+        public void Toca(IList<INota> musica)
+        {
+            foreach (INota nota in musica)
+            {
+                Console.Beep(nota.Frequencia, 300);
+            }
+        }
+    }
+}
--- DesignPatterns2/Program.cs
+++ DesignPatterns2/Program.cs
@@ -1,7 +1,26 @@
 ﻿using DesignPatterns2.Cap1;
+using DesignPatterns2.Cap2;
 using System.Data;
 
+/* Factory Method */
 IDbConnection conexao = new ConnectionFactory().GetConnection();
 
-IDbCommand comando =  conexao.CreateCommand();
+IDbCommand comando = conexao.CreateCommand();
 comando.CommandText = "select * from table";
+
+/* Flyweight */
+
+NotasMusicais notas = new NotasMusicais();
+
+IList<INota> musica = new List<INota>()
+{
+    notas.Pega("do"),
+    notas.Pega("re"),
+    notas.Pega("mi"),
+    notas.Pega("fa"),
+    notas.Pega("fa"),
+    notas.Pega("fa"),
+};
+
+Piano piano = new Piano();
+piano.Toca(musica);
